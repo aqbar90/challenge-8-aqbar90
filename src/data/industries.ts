a257acfdb4c00f4c4bc1e5d92a1b656e436306af
aqbar90 <muhammad.aqbar.90@gmail.com>
2026-05-30 21:26:33 +0700

    feat:Industry Section

---
+++ src/data/industries.ts
@@ -0,0 +1,29 @@
+import fintechImage from '../assets/Image/Industries/fintech.jpg';
+import ecommerceImage from '../assets/Image/Industries/E-Commerce.jpg';
+import healthcareImage from '../assets/Image/Industries/Healthcare.jpg';
+
+export const industriesData = [
+  {
+    id: 1,
+    title: 'Fintech',
+    description:
+      'We build secure, scalable, and compliant fintech solutions — from digital wallets to core banking systems — tailored to modern financial needs.',
+    image: fintechImage,
+  },
+
+  {
+    id: 2,
+    title: 'E-Commerce',
+    description:
+      'Boost your online sales with fast, reliable platforms designed for seamless shopping experiences, inventory management, and payment integration.',
+    image: ecommerceImage,
+  },
+
+  {
+    id: 3,
+    title: 'Healthcare',
+    description:
+      'Empowering healthcare providers with digital solutions that improve patient care, ensure data privacy, and streamline operational workflows..',
+    image: healthcareImage,
+  },
+];
